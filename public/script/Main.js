71dd8e72165e898dd7c316a2e9bee83f7184aad3
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-04-24 21:01:25 -0400

    setup popstate event, error when page not found

--- public/script/Main.js
+++ public/script/Main.js
@@ -2,13 +2,29 @@
 	Distributed under the terms of the GNU Affero General Public License v3
 */
 
+function page() {
+	return (window.location.pathname !== '/' ? window.location.pathname.split('/')[1] : 'Home');
+}
+
+function load(where) {
+	Nav.clear();
+	Nav.pages[where].get();
+}
+
+window.onpopstate = function(e) {
+	load(page());
+}
+
 window.addEvent('domready', function() {
 	const Nav = new Navigation($('menu'), $('main'), 'default');
 
 	Nav.addEvent('complete', function() {
-		const page = (window.location.pathname !== '/' ? window.location.pathname.split('/')[1] : 'Home');
-		console.log('on page ' + Nav.pages[page].name);
-		Nav.pages[page].get();
+		if(Nav.pages[page()] !== undefined) {
+			console.log('on page ' + Nav.pages[page()].name);
+			Nav.pages[page()].get();
+		} else {
+			$('main').set('html', 'Page not found');
+		}
 	});
 	$('home').addEvent('click', function() {
 		Nav.clear();
