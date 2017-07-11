a29f24739a546022b8f774e324802c774dd918e6
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-07-11 01:23:53 -0400

    fix typoe in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: 'static',
 	port: process.env.PORT || 8080,
 	config: {
-		host: 'localhost' || process.env.CONFIG_HOST,
-		port: 8081 || process.env.CONFIG_PORT,
+		host: process.env.CONFIG_HOST || 'localhost',
+		port: process.env.CONFIG_PORT || 8081,
 	},
 };
